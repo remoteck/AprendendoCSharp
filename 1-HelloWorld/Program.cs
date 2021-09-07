cd8613bd971a3d924821c17bbb99a4510e164eb3
Guilherme Gaspar Perroni <guilhermegasparperroni@hotmail.com>
2021-09-07 14:58:49 -0300

    Add project files.

---
+++ 1-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nPress enter to exit. . .");
+            Console.ReadLine();
+        }
+    }
+}
